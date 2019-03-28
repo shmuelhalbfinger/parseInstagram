0b9d8a3a00344e9ffd02e858300297f8ed3ad910
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2019-03-28 19:03:37 -0400

    Added the swipe refresh

--- app/src/main/java/com/example/parseinstagram/Post.java
+++ app/src/main/java/com/example/parseinstagram/Post.java
@@ -8,16 +8,17 @@ import com.parse.ParseUser;
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
-    public static final String KEY_Description = "description";
+    public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     public String getDescription() {
-        return getString(KEY_Description);
+        return getString(KEY_DESCRIPTION);
     }
 
     public void setDescription(String description) {
-        put(KEY_Description, description);
+        put(KEY_DESCRIPTION, description);
     }
 
     public ParseFile getImage() {
